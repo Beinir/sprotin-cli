2dd15b112ae5b8d9227df56689c1543480cad7e3
Beinir Ragnuson <beinir.ragnuson@gmail.com>
2020-02-27 22:27:54 +0100

    Else statement

--- sprotin.py
+++ sprotin.py
@@ -13,6 +13,8 @@ def search(searchString1, searchString2):
         webbrowser.open('https://sprotin.fo/dictionaries?_SearchInflections=0&_SearchDescriptions=0&_SearchFor=' + str(
             searchString1) +
                         '&_DictionaryPage=1&_DictionaryId=5&_Group=')
+    else:
+        print("Error: You have typed something wrong." "\n" "Try: en or da as your last parameter.")
 
 
 search(searchString1, searchString2)
